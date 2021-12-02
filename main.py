98fb52cf0f5c5388bc3e07bb31e033d95bf7ef44
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2021-12-02 11:28:50 +0100

    open(mode='r') is default (see: pydoc open)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Day1 import Day1
 
 if __name__ == '__main__':
     day1 = Day1()
-    file = open('day1.txt', mode='r')
+    file = open('day1.txt')
     day1Input = file.read()
     print("Day 1a " + str(day1.count(day1Input)) + "\n")
     print("Day 1b " + str(day1.count2(day1Input)) + "\n")
