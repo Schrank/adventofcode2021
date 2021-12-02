ff48d5400551a51da6fc8987869b8c3b6668732d
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2021-12-02 13:00:26 +0100

    print accepts multiple arguments and implicitly casts int to str

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     day1 = Day1()
     with open('day1.txt') as file:
         day1Input = file.read()
-    print("Day 1a " + str(day1.count(day1Input)) + "\n")
-    print("Day 1b " + str(day1.count2(day1Input)) + "\n")
+    print("Day 1a ", day1.count(day1Input), "\n")
+    print("Day 1b ", day1.count2(day1Input), "\n")
